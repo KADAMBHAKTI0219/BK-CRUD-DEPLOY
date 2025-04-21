12666a901dfc0b32b2e95f8c530d98754a408ee5
KADAMBHAKTI0219 <kadambhakti0219@example.com>
2025-04-21 18:17:08 +0530

    completed backend

---
+++ config/db.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+const dotenv = require('dotenv')
+dotenv.config();
+const connectionToDB = mongoose.connect(process.env.MONGO_DB_URI);
+module.exports = connectionToDB;
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv= require('dotenv');
+const path = require('path');
+const connectionToDB = require('./config/db');
+const ProductRouter = require('./routes/productRoutes');
+dotenv.config()
+const app = express()
+app.use(express.json());
+app.use(cors({
+    origin:  ["http://localhost:5173","http://localhost:5174","http://localhost:5175","http://localhost:3000","http://localhost:3001","http://localhost:3002"],   
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credential:true
+}))
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/products',ProductRouter)
+
+app.listen(process.env.PORT || 3000,async()=>{
+    try {
+        await connectionToDB;
+        console.log(`Server is running on Port ${process.env.PORT || 3000}`);
+    } catch (error) {
+        console.log(error)
+    }
+})
