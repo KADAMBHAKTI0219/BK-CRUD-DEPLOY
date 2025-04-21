8e02142ee7fc3e77c373e27aa68bd244d326d356
KADAMBHAKTI0219 <kadambhakti0219@example.com>
2025-04-21 19:01:41 +0530

    compleed:

--- config/db.js
+++ config/db.js
@@ -1,5 +1,23 @@
-const mongoose = require("mongoose");
-const dotenv = require('dotenv')
-dotenv.config();
-const connectionToDB = mongoose.connect(process.env.MONGO_DB_URI, { serverSelectionTimeoutMS: 100000000 });
-module.exports = connectionToDB;
+// const mongoose = require("mongoose");
+// const dotenv = require('dotenv')
+// dotenv.config();
+// const connectionToDB = mongoose.connect(process.env.MONGO_DB_URI);
+// module.exports = connectionToDB;
+const connectToDb = async () => 
+    {
+        try 
+        {
+            await mongoose.connect(process.env.MONGO_URI, 
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
+            console.log("connected to db");
+        }
+        catch (error) 
+        {
+            console.log(error);
+        }
+    }
+
+    module.exports = {connectToDb};
--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const dotenv= require('dotenv');
 const path = require('path');
-const connectionToDB = require('./config/db');
 const ProductRouter = require('./routes/productRoutes');
+const { connectToDb } = require('./config/db');
 dotenv.config()
 const app = express()
 app.use(express.json());
@@ -15,11 +15,9 @@ app.use(cors({
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/products',ProductRouter)
 
+connectToDb()
+
 app.listen(process.env.PORT || 3000,async()=>{
-    try {
-        await connectionToDB;
+    
         console.log(`Server is running on Port ${process.env.PORT || 3000}`);
-    } catch (error) {
-        console.log(error)
-    }
 })
