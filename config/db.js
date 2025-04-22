c6eee4d2667d15abd473ea9043565acda5beae7e
KADAMBHAKTI0219 <kadambhakti0219@example.com>
2025-04-22 10:24:15 +0530

    completed

--- config/db.js
+++ config/db.js
@@ -2,4 +2,4 @@ const mongoose = require("mongoose");
 const dotenv = require('dotenv')
 dotenv.config();
 const connectionToDB = mongoose.connect(process.env.MONGO_DB_URI);
-module.exports = {connectionToDB};
+module.exports = connectionToDB;
