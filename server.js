bc480cc6f868068f4c44a9cf2cd42aca7bbed7b1
KADAMBHAKTI0219 <kadambhakti0219@example.com>
2025-04-22 12:51:12 +0530

    completed

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
 
 // MongoDB connection
